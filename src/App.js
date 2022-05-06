fa45366bfe45c9b5ef933938cd6b44c6319c0f62
MarcoMorales19 <47836903+MarcoMorales19@users.noreply.github.com>
2022-05-06 18:15:28 -0500

    a

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import {AppBar, Toolbar, Typography, Button, IconButton, Grid,
+  Card, CardActions, CardContent   
+} from '@mui/material';
 
 import Home from './views/Home';
 import News from './views/News';
+
 
 // const CriptoChart = React.lazy(() => import('MF2/MF2'));
 // const CriptoNews = React.lazy(() => import('NEWSCRIPTOS/CriptoNews'));
@@ -10,21 +14,50 @@ import News from './views/News';
 const App = () => {
   return (
     <Fragment>
-      <h1>Welcome to Cripto World!</h1>
-
       <BrowserRouter>
-        <nav>
-          {/* <Link to={ '/' }> Home </Link> */}
-          <a href='/'> Home </a>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              =
+            </IconButton>
 
-          <Link to={ '/news' }> News </Link>
-        </nav>
+            <Typography variant="h5" component="div" sx={{ marginRight: '10px', marginLeft: '10px' }}>
+              Cripto World
+            </Typography>
+            <a href='/'>
+              <Typography variant="h6" component="div" sx={{ marginRight: '10px', marginLeft: '10px'  }}>
+                Home
+              </Typography>
+            </a>
+            <Link to={ '/news' }>
+              <Typography variant="h6" component="div" sx={{ marginRight: '10px' }}>
+                News
+              </Typography>
+            </Link>
+            
+            <Button color="inherit" sx={{ position: 'absolute', right: '40px' }}>Login</Button>
+          </Toolbar>
+        </AppBar>
+
+        {/* <nav> */}
+          {/* <Link to={ '/' }> Home </Link> */}
+          {/* <a href='/'> Home </a>
+
+          <Link to={ '/news' }> News </Link> */}
+        {/* </nav> */}
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/news" element={<News />} />
         </Routes>
       </BrowserRouter>
+      
     </Fragment>
   );
 };
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,5 +1,8 @@
 import React, { Fragment } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
+import {AppBar, Toolbar, Typography, Button, IconButton, Grid,
+  Card, CardActions, CardContent   
+} from '@mui/material';
 
 import ErrorFallback from './../../components/ErrorFallback';
 
@@ -13,7 +16,46 @@ export default function index () {
           FallbackComponent={ErrorFallback}
           onReset={() => {}}
         >
-          <CriptosDetails />
+          <>
+          
+            
+      <Grid container width="100%" style={{padding: '2rem'}} spacing={2}>
+
+        <Grid item xs={12} sm={12} md={8} lg={8} xl={8} style={{ marginTop: '2rem', justifyContent: 'center', alignItems: 'center'}}>
+          <Typography color='rgb(0, 127, 255)' variant="h2" component="div" >
+            Start now!
+          </Typography>
+          <Typography color='black' variant="h3" component="div">
+            Start building your cryptocurrency portfolio
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} sm={12} md={4} lg={4} xl={4} style={{ marginTop: '2rem'}}>
+
+          <CardContent style={{ borderRadius: '5px', background: 'rgb(0, 89, 178) 120%', height: '25rem', alignContent: 'center', alignItems: 'center', justifyContent: 'center' }}>
+            <Typography variant="h4" color="white" gutterBottom>
+              Save your money
+            </Typography>
+            <Typography variant="h5" component="div" color="white">
+              gdfgggd
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="white">
+              adjective
+            </Typography>
+          </CardContent>
+          {/* <CardActions style={{ borderRadius: '5px', background: 'rgb(0, 89, 178) 120%' }}>
+            <Button size="small">Learn More</Button>
+          </CardActions> */}
+
+        </Grid>
+
+      </Grid>
+
+
+
+            {/* <CriptosDetails /> */}
+
+          </>
         </ErrorBoundary>
       </React.Suspense>
     </Fragment>
